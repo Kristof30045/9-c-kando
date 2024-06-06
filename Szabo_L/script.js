750503e26649e94ffc6a1777e5070129f529215b
szorostalpu12 <szabol1@kkszki.hu>
2024-06-06 22:26:36 +0200

    változas

--- Szabo_L/script.js
+++ Szabo_L/script.js
@@ -9,3 +9,19 @@ function openModal() {
 
     dialogus.close();
   }
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    let pass = event.target.password.value;
+    let email = event.target.email.value;
+
+    if (pass && email) {
+      alert("Sikeresen feliratkoztál hírlevelünkre!");
+      event.target.submit();
+    }
+    else
+      alert("Töltsd ki a mezőket!");
+  }
+
+  document.getElementById("sub").addEventListener("submit", onSubmit);
