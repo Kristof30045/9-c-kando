824b84b464f6c2b41177838031973e59faa971f1
szorostalpu12 <szabol1@kkszki.hu>
2024-06-06 21:59:23 +0200

    bővítés

---
+++ Szabo_L/script.js
@@ -0,0 +1,11 @@
+function openModal() {
+    let dialogus = document.getElementById("dialogus");
+
+    dialogus.showModal();
+  }
+
+  function closeModal() {
+    let dialogus = document.getElementById("dialogus");
+
+    dialogus.close();
+  }
